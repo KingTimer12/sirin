c3ce2c312b1d29a1529b6747b8705a6abb345306
KingTimer12 <queentimer12@gmail.com>
2024-03-10 10:50:08 -0300

    fix: fixed punctuation

--- src/ast/lexer.rs
+++ src/ast/lexer.rs
@@ -2,7 +2,7 @@
 pub enum TokenKind {
     Number(i64),
     Plus,
-    Menus,
+    Minus,
     Asterisk,
     Slash,
     LeftParen,
@@ -62,26 +62,43 @@ impl<'a> Lexer<'a> {
             return Some(Token::new(TokenKind::Eof, TextSpan::new(0, 0, eof_char.to_string())))
         }
 
-        let start = self.cur_pos;
         let c = self.current_char();
-        let mut kind = TokenKind::Bad;
-        if Self::is_number_start(&c) {
-            let number: i64 = self.consume_number();
-            kind = TokenKind::Number(number)
-        }
+        c.map(|c| {
+            let start = self.cur_pos;
+            let mut kind = TokenKind::Bad;
+            if Self::is_number_start(&c) {
+                let number: i64 = self.consume_number();
+                kind = TokenKind::Number(number)
+            } else {
+                kind = self.consume_punctuation();
+            }
 
-        let end = self.cur_pos;
-        let literal = self.input[start..end].to_string();
-        let span = TextSpan::new(start, end, literal);
-        Some(Token::new(kind, span)) // return Token
+            let end = self.cur_pos;
+            let literal = self.input[start..end].to_string();
+            let span = TextSpan::new(start, end, literal);
+            Token::new(kind, span) // return Token
+        }) // return Option<Token>
+    }
+
+    fn consume_punctuation(&mut self) -> TokenKind {
+        let c = self.consume().unwrap();
+        match c {
+            '+' => TokenKind::Plus,
+            '-' => TokenKind::Minus,
+            '*' => TokenKind::Asterisk,
+            '/' => TokenKind::Slash,
+            '(' => TokenKind::LeftParen,
+            ')' => TokenKind::RightParen,
+            _ => TokenKind::Bad
+        }
     }
 
     fn is_number_start(c: &char) -> bool {
         c.is_digit(10) // return bool
     }
 
-    fn current_char(&self) -> char {
-        self.input.chars().nth(self.cur_pos).unwrap() // return char
+    fn current_char(&self) -> Option<char> {
+        self.input.chars().nth(self.cur_pos) // return Option<char>
     }
 
     fn consume(&mut self) -> Option<char> {
@@ -90,13 +107,14 @@ impl<'a> Lexer<'a> {
         }
         let c = self.current_char();
         self.cur_pos += 1;
-        Some(c) // return Option<char>
+        c // return Option<char>
     }
 
     fn consume_number(&mut self) -> i64 {
         let mut number: i64 = 0;
-        while let Some(c) = self.consume() {
+        while let Some(c) = self.current_char() {
             if Self::is_number_start(&c) {
+                self.consume().unwrap();
                 number = number * 10 + c.to_digit(10).unwrap() as i64;
             } else {
                 break;
